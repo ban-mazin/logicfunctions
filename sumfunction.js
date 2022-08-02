01dd2980e08d99c1fdd0203934e35517701218f3
ban-mazin <ban.qaddoori89@gmail.com>
2022-08-02 09:46:47 -0600

    ccount number of item on the list

---
+++ sumfunction.js
@@ -0,0 +1,24 @@
+/* Question 0
+ *
+ *  Implement the functions defined below
+ *
+ */
+
+//const { isLength, result } = require("lodash");
+
+/* ===========================================================================
+ * COUNT - the number of items in a list
+ *
+ * For example:
+ *
+ *    count([6,2,3,4,9,6,1,0,5]);
+ *
+ * Returns:
+ *
+ *    9
+ */
+/* IMPLEMENT ME */
+const num = function(count) {
+    return count.length;
+    console.log(count.length);
+}

b084c594ef26339c08d1030a5cd4e71e0912abf3
ban-mazin <ban.qaddoori89@gmail.com>
2022-08-02 10:27:47 -0600

    the sum of the number in a list

--- sumfunction.js
+++ sumfunction.js
@@ -23,3 +23,25 @@ const count = function(arr) {
     
 };
 console.log(count([6, 2, 3, 4, 9, 6, 1, 0, 6]));
+
+/* SUM - the sum of the numbers in a list
+       - safe to assume that all items are numbers already
+ *
+ * For example:
+ *
+ *    sum([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    36
+ */
+/* IMPLEMENT ME */
+const sum = function(arra) {
+    let result = 0;
+    for (let i = 0; i < arra.length; i++) {
+        result += arra[i];
+        
+    }
+    return result;
+};
+console.log(sum([6, 2, 3, 4, 9, 6, 1, 0, 6]));

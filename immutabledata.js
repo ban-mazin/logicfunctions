a7cf4fde6c2180e62c2daa455b9515a3c6ccd4ec
ban-mazin <ban.qaddoori89@gmail.com>
2022-08-24 12:23:09 -0600

    add commit

--- immutabledata.js
+++ immutabledata.js
@@ -1,13 +1,13 @@
 
-
+// copy an array with the spread operator
 const myArr = [1, 2, 3];
 const copy = [...myArr];
 copy.push(4);
-
+// works the same for objects
 const myObj = {name: 'Alice'};
 const newObj = {...myObj};
 newObj.name = 'Bob';
-
+// it's possible to overwrite keys in a single step
 const myObj = {name:'Alicce', age:27};
 const newObj= {...myObj, name: 'Bob'};
 console.log(newObj);

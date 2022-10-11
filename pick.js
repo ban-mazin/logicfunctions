220e18a7eba2342fac0e3e8085f5712e668970fd
ban-mazin <ban.qaddoori89@gmail.com>
2022-10-11 11:56:01 -0600

    practicing js

--- pick.js
+++ pick.js
@@ -3,6 +3,15 @@
 //}
 //setTimeout(pick, 5000);
 
-setTimeout(function(){
-  alert("welcome")
-}, 5000)
+// setTimeout(function(){
+//   alert("welcome")
+// }, 5000)
+
+const numbers = [2, 10, 3, 7, 8];
+// numbers.forEach(function(num) {
+//   console.log(num);
+// })
+function printTriple(n) {
+  console.log(n * 3);
+}
+numbers.forEach(printTriple);

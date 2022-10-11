e9c4e06a2f791dae352f618466e8738101bfb287
ban-mazin <ban.qaddoori89@gmail.com>
2022-10-11 13:20:21 -0600

    practicing js

--- pick.js
+++ pick.js
@@ -7,11 +7,25 @@
 //   alert("welcome")
 // }, 5000)
 
-const numbers = [2, 10, 3, 7, 8];
-// numbers.forEach(function(num) {
-//   console.log(num);
-// })
-function printTriple(n) {
-  console.log(n * 3);
-}
-numbers.forEach(printTriple);
+// const numbers = [2, 10, 3, 7, 8];
+// // numbers.forEach(function(num) {
+// //   console.log(num);
+// // })
+// function printTriple(n) {
+//   console.log(n * 3);
+// }
+// numbers.forEach(printTriple);
+
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+const words = ['asap', 'byob', 'rsvp', 'dgfd'];
+
+const m = numbers.map(function(num){
+  return num * 2
+})
+console.log(m);
+
+const breave = words.map(function(word){
+  return word.toUpperCase().split('').join('.');
+})
+console.log(breave);
+

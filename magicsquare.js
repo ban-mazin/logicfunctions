861da8c3691f2f437133e19cbbd3e469c6d4aaa1
ban-mazin <ban.qaddoori89@gmail.com>
2022-10-24 20:04:18 -0600

    practice js

--- magicsquare.js
+++ magicsquare.js
@@ -1,9 +1,35 @@
-const magic = [[2, 7, 6], [9, 5, 1], [4, 3, 8]];
+// const magic = [[2, 7, 6], [9, 5, 1], [4, 3, 8]];
 
-for(let row of magic) {
-  let sum = 0;
-  for(let num of row) {
-    sum += num;
-  }
-  console.log(`${row} summed to ${sum}`);
+// for(let row of magic) {
+//   let sum = 0;
+//   for(let num of row) {
+//     sum += num;
+//   }
+//   console.log(`${row} summed to ${sum}`);
+// }
+
+const movieReviews = {
+  arrival: 9.5,
+  alien: 9,
+  amelie: 8,
+  'In Bruges': 10
 }
+
+//  for(let movie of Object.keys(movieReviews)) {
+//    console.log(movie);
+
+// }
+  // console.log(Object.keys(movieReviews));
+  // console.log(Object.values(movieReviews))
+
+  // const rating = Object.values(movieReviews);
+  // let total = 0;
+  // for(let r of rating) {
+  //   total += r;
+    
+  // }
+  for (let movie in movieReviews) {
+    console.log(movie)
+    console.log(movieReviews[movie])
+  }
+
